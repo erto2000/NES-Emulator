f034371697f27c1bbd56fedf7ed25f0d8f4e92c9
cakircay <cakircays@gmail.com>
2022-01-17 22:58:42 +0300

    More Clear

--- Design/Timing_Logic.vhd
+++ Design/Timing_Logic.vhd
@@ -22,35 +22,13 @@ entity Timing_Logic is
 end Timing_Logic;
 
 architecture Behavioral of Timing_Logic is
-    signal next_cycle            : integer range 0 to 7;
     signal cycle_reg             : integer range 0 to 7 := 0;
-    signal next_IR               : std_logic_vector(DATA_WIDTH-1 downto 0);
     signal IR                    : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
 begin
     cycle <= cycle_reg;
     instruction <= IR;
     sync <= cycle_reset; --Inform user of the cpu while fetching opcode
-
-    --Determine next cycle based on the control input
-    next_cycle <= 0 when cycle_reset = '1' else
-                  cycle_reg + 1 when cycle_increment = '1' else
-                  cycle_reg + 2 when cycle_skip = '1' else
-                  cycle_reg + 3 when cycle_double_skip = '1' else
-                  cycle_reg;
-    
-    --Fetch new opcode or start interrupt or do nothing
-    process(next_cycle, irq_flag, nmi_flag, PD, IR) begin
-        if(next_cycle = 0) then
-            if(irq_flag = '1' or nmi_flag = '1') then
-                next_IR <= (others => '0'); --BRK instruction
-            else
-                next_IR <= PD;
-            end if;
-        else
-            next_IR <= IR;
-        end if;
-    end process;
-    
+                                   
     --Change cycle and instruction on clk_ph1
     process(clk) begin
         if(rising_edge(clk) and clk_ph1 = '0') then --rising edge ph1
@@ -58,8 +36,14 @@ begin
                 cycle_reg <= 0;
                 IR <= (others => '0');
             else
-                cycle_reg <= next_cycle;
-                IR <= next_IR;
+                cycle_reg <= 0 when cycle_reset = '1' else                     
+                             cycle_reg + 1 when cycle_increment = '1' else     
+                             cycle_reg + 2 when cycle_skip = '1' else          
+                             cycle_reg + 3 when cycle_double_skip = '1' else   
+                             cycle_reg;   
+               
+                IR <= (others => '0') when (cycle_reset and (irq_flag or nmi_flag)) = '1' else                     
+                      PD when cycle_reset = '1';                                                
             end if;  
         end if; 
     end process;
